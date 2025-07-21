8628af8bace01bab7079a1adcd17560a47ef61d0
ravi-nan-1 <singhrajeev1470@gmail.com>
2025-07-21 15:30:29 +0530

    Update MachineAlgo.py

--- backend/MachineAlgo.py
+++ backend/MachineAlgo.py
@@ -118,7 +118,7 @@ def super_trend(data):
     import pandas_ta as ta
     import numpy as np
     import pandas as pd
-    data['st_sig'=0]
+    data['st_sig']=0
 
 
     # Helper: check if open and close are inside any zone
